84f0eb30c9ee156762fdde193d4557b16a285736
jian <1056356544@qq.com>
2016-10-08 11:34:10 +0800

    delete commit

---
+++ j2/e.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * php键哥专用.
+ * User: duang.
+ * Date: 2016/10/8
+ * Time: 11:32
+ */
+header("Content-type:text/html;charset=utf-8");
+class test
+{
+    function delete()
+    {
+        echo 'deldte';
+    }
+}
+?>
