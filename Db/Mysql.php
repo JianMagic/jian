f4a8b77bcb9e0b337c02333244c3489a42476faf
chen <1056356544@qq.com>
2016-10-08 14:42:58 +0800

    mysql

--- Db/Mysql.php
+++ Db/Mysql.php
@@ -1,6 +1,22 @@
 <?php
-namespace Db;
-class Mysql
-{
 
+namespace Db;
+class Mysql implements IDatabase
+{
+    function connect()
+    {
+        // TODO: Implement connect() method.
+
+    }
+
+    function query()
+    {
+        // TODO: Implement query() method.
+
+    }
+
+    function close()
+    {
+        // TODO: Implement close() method.
+    }
 }
