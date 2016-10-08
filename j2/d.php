d9ed06b44f6aa5770448968e5d68a249b7bd26ef
jian <1056356544@qq.com>
2016-10-08 11:32:58 +0800

    第二次提交

---
+++ j2/d.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * php键哥专用.
+ * User: duang.
+ * Date: 2016/10/8
+ * Time: 11:31
+ */
+header("Content-type:text/html;charset=utf-8");
+class test
+{
+    function add()
+    {
+
+    }
+}
+?>
