1b11385d868a928a8d3898b670441682dc6476d0
chen <1056356544@qq.com>
2016-10-08 14:46:36 +0800

    mysql

--- Db/PDO.php
+++ Db/PDO.php
@@ -1,17 +1,27 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: HY
+ * Date: 2016/10/8
+ * Time: 13:15
+ */
+//header("Content-type:text/html;charset=utf-8");
 namespace Db;
-class PDO
+class PDO implements IDatabase
 {
     function connect()
     {
-        
+        // TODO: Implement connect() method.
     }
+
     function query()
     {
-        
+        // TODO: Implement query() method.
     }
+
     function close()
     {
-        
+        // TODO: Implement close() method.
     }
+
 }
